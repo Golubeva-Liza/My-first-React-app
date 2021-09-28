d11374bfc945ebc3977fd26caff3907cd9e2822e
LizaGolubeva <sp6lisa@yandex.ru>
2021-09-28 18:28:18 +0300

    react компоненты

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const Header = () => {
+  return <h2>Hello mama</h2>
+}
+// const Field = () => {
+//   const holder = "Enter here"
+//   const styledFiled = {
+//     width: '300px'
+//   }
+//   return <input placeholder={holder} type="text" style={styledFiled}/>
+// }
+
+//компонент созданный с помощью класса
+class Field extends React.Component{//из реакт компонент мы берем все базовые вещи и наследуем в наш класс
+  render(){//обязательно должен быть метод render
+    const holder = "Enter here"
+    const styledFiled = {
+      width: '200px'
+    }
+    return <input placeholder={holder} type="text" style={styledFiled}/>
+  }
+}
+
+function Btn () {
+  const text = "Login";
+  // const res = () => {
+  //   return "Login";
+  // }
+  const logged = true;
+  //? - если да, : если нет
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
+
+//компонент
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
