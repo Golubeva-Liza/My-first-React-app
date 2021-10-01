ccb6de4df2eeffe48d621e79e93df9dd0c2c8661
LizaGolubeva <sp6lisa@yandex.ru>
2021-10-01 20:39:01 +0300

    использование ключа key в компонентах, что такое состояние компонента (на примере классового)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,27 +4,66 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import {Component} from 'react';
 
 import './app.css';
 
-//function WhoAmI(props){//props все равно будет сформирован, если даже его не прописать
-// function WhoAmI({name, surname, link}){//чаще пишут так - деструктуризация
-//     return (
-//         <div>
-//             <h1>My name is {name()}, surname - {surname}</h1>
-//             <a href={link}>My profile</a>
-//         </div>
-//     )
-// }
+
+class WhoAmI extends Component{
+    constructor(props){
+        super(props);
+        //в таком виде конструктор можно убрать. но если в него надо что-то поместить, то другое дело
+        //В конструкторе ключевое слово super() используется как функция, вызывающая родительский конструктор. Её необходимо вызвать до первого обращения к ключевому слову this в теле конструктора.
+        
+        //создаем состояние
+        this.state = {
+            years: 27, 
+            text: 'что-то'//если изменения этого не прописаны в setState, а там возвращается новый объект, то с этим свойством ничего не будет, тк так устроен setState
+            //setState меняет только то, что в ней прописано, остальное оставляет неизменным
+        }
+    }
+
+    nextYear = () => {
+        console.log('++');
+        // this.state.years++; - напрямую состояния менять нельзя
+        // this.setState({
+        //     // years: ++this.state.years - тут инкремент лучше не использовать, тк все равно меняется напрямую состояние
+        //     years: this.state.years + 1
+        // }); // правильный способ
+        //запускает перерисовку всего компонента с новым состоянием (вызов метода render)
+        
+        // setState - асинхронная операция. Если много раз кликать кнопку сверхбыстро, то предыдущ значение может сбиться (не успеть выполниться предыдущ операция, как пошла уже следующая)
+        // решение - callback функция. но когда не важно предыдущее состояние, то можно написать первый вариант (см выше)
+
+        this.setState(state => ({//state-текущее на данным момент состояние
+            years: state.years + 1
+        }));// синтаксис оборота объекта в круглые скобки = вернуть объект в результате выполнения
+        
+    }
+    //здесь только стрелочная функция (в реакт)
+    
+    render(){
+        const {name, surname, link} = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                <h1>My name is {name}, surname - {surname}, age - {this.state.years} </h1>
+                <a href={link}>My profile</a>
+            </div>
+        )
+    }
+    //класс создает экземпляры
+    //передаем кнопке метод - не вызываем, а указываем
+}
 
 
 function App(){
     // имитируем данные с сервера
     const data = [
-        {name: "John G.", salary:800, increase: true},
-        {name: "Seva G.", salary:3000, increase: false},
-        {name: "Lera S.", salary:4000, increase: false},
-        {name: "Max K.", salary:1500, increase: true},
+        {name: "John G.", salary:800, increase: true, id: 1},
+        {name: "Seva G.", salary:3000, increase: false, id: 2},
+        {name: "Lera S.", salary:4000, increase: false, id: 3},
+        {name: "Max K.", salary:1500, increase: true, id: 4},
     ];
 
     return (
@@ -37,6 +76,9 @@ function App(){
             </div>
             <EmployeesList data={data}/>
             <EmployeesAddForm/>
+
+            <WhoAmI name="John" surname = "Smith" link="facebook.com"/>
+            <WhoAmI name="Liza" surname = "Golubeva" link="vk.com"/>
         </div>
     );
 }
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,8 +4,17 @@ import './employees-list.css';
 const EmployersList = ({data}) => {
     // перебирается массив data и возвращается новый
     const elements = data.map(item => {
+        // можно сделать так
+        // const {id, ...itemProps} = item;
+        // return (
+        //     <EmployeesListItem key={id} {...itemProps}/>
+        //     // <EmployeesListItem {...item} /> - то же самое
+        // )
+        //если с бэка не пришел id, то можно подставить в key так
+        // const elements = data.map((item, id) => {
+        //такое допускается, если они не будут меняться местами!
         return (
-            <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase}/>
+            <EmployeesListItem key={item.id} name={item.name} salary={item.salary} increase={item.increase}/>
             // <EmployeesListItem {...item} /> - то же самое
         )
     });
