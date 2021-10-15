5167539146c1a36f4d0d943364f8ec84cd9d65e2
LizaGolubeva <sp6lisa@yandex.ru>
2021-10-16 00:31:30 +0300

    Last commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';//отвечает за работу с react, jsx
 import ReactDOM from 'react-dom';//позволяет работать с dom структурой на странице.
-//вставляет библиотеку react и наш продукт на страницу
+
 import './index.css';
 import App from './components/app/app';
 
@@ -10,10 +10,9 @@ import App from './components/app/app';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,//что мы рендерим на странице
-  document.getElementById('root')//во что мы помещаем (место в index.html)
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-//компоненты реакта пишутся с большой буквы
 //<React.StrictMode> - инструмент для распознавания потенциальных проблем в приложении
 //активирует доп проверки. помогает обнаружить устаревшие или небезопасные конструкции
